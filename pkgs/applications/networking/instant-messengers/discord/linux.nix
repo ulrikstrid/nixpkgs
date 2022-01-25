2fd5dd75b0c630d7a15d87fa303bc0915aa2b743
Ulrik Strid <ulrik.strid@outlook.com>
2022-01-25 08:15:56 +0100

    Add wayland flags to discord

--- pkgs/applications/networking/instant-messengers/discord/linux.nix
+++ pkgs/applications/networking/instant-messengers/discord/linux.nix
@@ -81,7 +81,8 @@ stdenv.mkDerivation rec {
     wrapProgram $out/opt/${binaryName}/${binaryName} \
         "''${gappsWrapperArgs[@]}" \
         --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}/" \
-        --prefix LD_LIBRARY_PATH : ${libPath}:$out/opt/${binaryName}
+        --prefix LD_LIBRARY_PATH : ${libPath}:$out/opt/${binaryName} \
+        --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
 
     ln -s $out/opt/${binaryName}/${binaryName} $out/bin/
     # Without || true the install would fail on case-insensitive filesystems

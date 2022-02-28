b20ddcc6e0e931127e2b03a75b8cba21ada50fe7
Ulrik Strid <ulrik.strid@outlook.com>
2022-02-28 15:29:36 +0100

    ligo: 0.34.0 -> 0.36.0
    Also changes to use the vendored tezos instead

--- pkgs/development/compilers/ligo/default.nix
+++ pkgs/development/compilers/ligo/default.nix
@@ -1,23 +1,34 @@
 { lib
 , fetchFromGitLab
+, git
 , coq
 , cacert
 }:
 
 coq.ocamlPackages.buildDunePackage rec {
   pname = "ligo";
-  version = "0.34.0";
+  version = "0.36.0";
   src = fetchFromGitLab {
     owner = "ligolang";
     repo = "ligo";
     rev = version;
-    sha256 = "sha256-MHkIr+XkW/zrRt+Cg48q4fOWTkNGH0hbf+oU7cAivNE=";
+    sha256 = "sha256-aUN2ECSOO4a7GOrJMMTKaFyhU75f4/6ogp/Ur1ipS5A=";
+    fetchSubmodules = true;
   };
 
   # The build picks this up for ligo --version
   LIGO_VERSION = version;
 
   useDune2 = true;
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    git
+    coq
+    coq.ocamlPackages.menhir
+    coq.ocamlPackages.ocaml-recovery-parser
+  ];
 
   buildInputs = with coq.ocamlPackages; [
     coq
@@ -28,23 +39,34 @@ coq.ocamlPackages.buildDunePackage rec {
     ppx_deriving
     ppx_deriving_yojson
     ppx_expect
-    tezos-base
-    tezos-shell-services
-    tezos-010-PtGRANAD-test-helpers
-    tezos-011-PtHangz2-test-helpers
-    tezos-protocol-010-PtGRANAD-parameters
-    tezos-protocol-010-PtGRANAD
-    tezos-protocol-environment
-    yojson
-    getopt
+    ppx_import
     terminal_size
-    pprint
-    linenoise
-    data-encoding
-    bisect_ppx
-    cmdliner
-    core
     ocaml-recovery-parser
+    yojson
+
+    # Test helpers deps
+    qcheck
+    qcheck-alcotest
+    alcotest-lwt
+
+    # vendored tezos' deps
+    ctypes
+    hacl-star
+    hacl-star-raw
+    lwt-canceler
+    ipaddr
+    bls12-381-unix
+    bls12-381-legacy
+    ptime
+    mtime
+    lwt_log
+    ringo
+    ringo-lwt
+    secp256k1-internal
+    resto
+    resto-directory
+    resto-cohttp-self-serving-client
+    irmin-pack
   ];
 
   checkInputs = [

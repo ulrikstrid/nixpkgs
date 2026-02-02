9140e0e832ced9a7dc51c6daacfe4aa29d2afa26
Rafael Ieda <git@ieda.me>
2026-02-02 09:19:47 -0300

    bolt-launcher: update meta.changelog
    (cherry picked from commit b35efa241763cf82e5bcd767675c28525c3f1b17)

--- pkgs/by-name/bo/bolt-launcher/package.nix
+++ pkgs/by-name/bo/bolt-launcher/package.nix
@@ -148,6 +148,7 @@ buildFHSEnv {
 
   meta = {
     homepage = "https://codeberg.org/Adamcake/Bolt";
+    changelog = "https://codeberg.org/Adamcake/Bolt/releases/tag/${bolt.version}";
     description = "Alternative launcher for RuneScape";
     longDescription = ''
       Bolt Launcher supports HDOS/RuneLite by default with an optional feature flag for RS3 (enableRS3).
